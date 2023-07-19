76925bda96391a2eb8ed8b9e20b99fd1a1492793
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-07-20 02:36:49 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 21.07 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+        var answer = ArrayList<Int>()
+        arr.forEach{
+            if(it >= 50 && it%2 == 0) answer.add(it/2)
+            else if(it < 50 && it%2 != 0) answer.add(it*2)
+            else answer.add(it)
+        }
+        return answer.toIntArray()
+    }
+}
