fb9131f4b45f983fd22bbd0e3e3c94ba0b72a043
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-07-31 17:02:06 +0900

    [level 2] Title: 올바른 괄호, Time: 16.99 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>(); 
+        
+        for(int i=0; i<s.length(); i++){
+            char temp = s.charAt(i);
+            if( temp == '('){
+                stack.push(temp);
+            }else if(temp ==')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
