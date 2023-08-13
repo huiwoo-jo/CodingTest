d407245f64d62731805d282164bd2c3a849da1e6
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-08-14 05:04:31 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 359.91 ms, Memory: 58.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(balls: Int, share: Int): Int = combination(balls, share)
+    private fun combination(n: Int, r: Int): Int {
+        return if (r == 0 || n == r) 1
+            else combination(n - 1, r - 1) + combination(n - 1, r)
+    }
+}
