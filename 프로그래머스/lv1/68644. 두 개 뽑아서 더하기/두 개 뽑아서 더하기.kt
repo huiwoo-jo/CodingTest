e82b1e6979cd610bc5738d8bff3c3024002280bb
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-08-17 18:20:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 40.17 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        for(i in 0 until numbers.size){
+            for(j in i+1 until numbers.size){
+                answer += numbers[i] + numbers[j]
+            }
+        }
+        return answer.distinct().sorted().toIntArray()
+    }
+}
