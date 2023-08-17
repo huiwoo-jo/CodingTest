8e2ef37ea31bf77a76fd6b604161527f2132fa63
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-08-17 19:33:19 +0900

    [level 1] Title: 최소직사각형, Time: 3.86 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.kt
@@ -1,8 +1,5 @@
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
-        val weight = sizes.map{Math.min(it[0], it[1])}
-        val height = sizes.map{Math.max(it[0], it[1])}
-        
-        return weight.maxOf{it} * height.maxOf{it}
+        return sizes.map{Math.min(it[0], it[1])}.maxOf{it} * sizes.map{Math.max(it[0], it[1])}.maxOf{it}
     }
 }
