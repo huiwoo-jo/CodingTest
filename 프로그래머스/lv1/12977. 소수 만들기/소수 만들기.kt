d924dfa24d0e111a9afd567ec35da906f9c0f5e7
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-08-20 00:06:36 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 59.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun solution(nums: IntArray): Int {
+        var answer = 0
+        
+        for(i in 0 until nums.size-2){
+            for(j in i+1 until nums.size-1){
+                for(k in j+1 until nums.size){
+                    var sum = nums[i] + nums[j] + nums[k]
+                    var flag = 0
+                    
+                    for (item in 2 until sum) {
+                        if (sum % item == 0) {
+                            flag = 1
+                            break
+                        }
+                    }
+                    
+                    if (flag == 0) {
+                        answer += 1
+                    }
+                }
+            }
+        }
+
+        return answer
+    }
+}
