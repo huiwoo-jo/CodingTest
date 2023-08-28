ef1409c9d8549f3c73f5949d4e837afd713f826c
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2023-08-28 11:30:10 +0900

    [unrated] Title: 귤 고르기, Time: 81.23 ms, Memory: 93.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun solution(k: Int, tangerine: IntArray): Int =
+        tangerine.groupBy { it }.values.map{it.size}.sortedDescending()
+        .runningFold(0) { acc, i -> acc + i }
+        .indexOfFirst{it>=k}.takeIf{it!=-1} ?: tangerine.size
+}
